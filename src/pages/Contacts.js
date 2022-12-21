02d41c275f9aafe1a36509c462a2bf53b9364744
ioriqC <ioriqC@gmail.com>
2022-12-21 18:52:07 +0200

    first commit

---
+++ src/pages/Contacts.js
@@ -0,0 +1,27 @@
+const Contacts = () => {
+  return (
+    <main className="section">
+        <div className="container">
+                <h1 className="title-1">Contacts</h1>
+
+                <ul className="content-list">
+                    <li className="content-list__item">
+                        <h2 className="title-2">Location</h2>
+                        <p>Chisinau, Moldova</p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Telegram / WhatsApp</h2>
+                        <p><a href="tel:+37368000000">+373 (68) 00-00-00</a></p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Email</h2>
+                        <p><a href="mailto:webdev@protonmail.com">admin@admin.com</a></p>
+                    </li>
+                </ul>
+
+        </div>
+    </main>
+  );
+}
+ 
+export default Contacts;
